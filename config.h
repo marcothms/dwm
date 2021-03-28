ff345d3b171ebc86b0708f62d2aff5ed08ae617e
Marco Thomas <mail@marco-thomas.net>
2021-03-28 01:19:34 +0100

    Add Icon font

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 
-static const char *fonts[]          = { "Product Sans:size=10", "Stick:size=10" };
+static const char *fonts[]          = { "Product Sans:size=10", "Stick:size=10", "Material Design Icons:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
